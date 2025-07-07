aee2edcf03e430fd5d0f7cb5936288137e84821a
Praj1998 <prajwaljangale555@gmail.com>
2025-07-07 21:36:14 +0530

    setup and required packages are installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Prajwal Jangale",
+    author_email="prajwaljangale555@gmail.com",
+    packages=find_packages()
+)
