a13923724a304c84b7e9001a35a070c2ec5d6202
Praj1998 <prajwaljangale555@gmail.com>
2025-07-08 06:55:18 +0530

    mongoDB setup complete, loggign and exception tested

---
+++ demo.py
@@ -0,0 +1,22 @@
+# below code is to check the logging config
+import sys
+
+from src.exception import MyException
+# below code is to check the exception config
+from src.logger import logging
+
+# logging.debug("This is a debug message.")
+# logging.info("This is an info message.")
+# logging.warning("This is a warning message.")
+# logging.error("This is an error message.")
+# logging.critical("This is a critical message.")
+
+
+#---------------------------------------------------------------------
+
+
+try:
+    a = 1+'Z'
+except Exception as e:
+    logging.info(e)
+    raise MyException(e, sys) from e
